0c97b610ee34192aea007afb065b7042e0c03db3
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-01-17 14:57:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
 alert('Hello world!');
-let numUsu = prompt("Dame un numero:");
+let secretNum = 5;
+let numUsu = prompt("Me indicas un número entre 1 y 10 por favor:");
+console.log(numUsu);
+
+
+if (numUsu == secretNum) {
+    //cumple
+    alert(`Acertaste, el número es: ${numUsu}`);
+} else {
+    //no cumple
+    alert('Lo siento, no acertaste el número');
+}
+
+
+
+/*
+alert('¡Bienvenida y bienvenido a nuestro sitio web!');
+
+let nombre = "Luna";
+
+let edad = 25;
+
+let numeroDeVentas = 50;
+
+let saldoDisponible = 1000;
+
+alert('¡Error! Completa todos los campos');
+
+let mensajeDeError = '¡Error! Completa todos los campos';
+
+alert(mensajeDeError);
+
+nombre = prompt('Por favor, ingresa tu nombre:');
+
+edad = prompt('Por favor, ingresa tu edad:');
+if (edad >= 18) {
+  alert('¡Puedes obtener tu licencia de conducir!');
+}
+*/
