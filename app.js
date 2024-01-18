8d2b21f7ad06ca622ac8f32acf05d09a8be75592
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-01-17 22:49:27 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-alert('Hello world!');
+alert("Hello world!");
 let secretNum = 5;
-let numUsu = prompt("Me indicas un número entre 1 y 10 por favor:");
-console.log(numUsu);
+let intentos = 1;
+let numUsu = 0;
+let palabra = "vez";
 
-
-if (numUsu == secretNum) {
+while (condition) {
+  numUsu = prompt("Me indicas un número entre 1 y 10 por favor:");
+  console.log(numUsu);
+  if (numUsu == secretNum) {
     //cumple
-    alert(`Acertaste, el número es: ${numUsu}`);
-} else {
-    //no cumple
-    alert('Lo siento, no acertaste el número');
+    alert(`Acertaste, el número es: ${numUsu}.  Lo hiciste en ${intentos} ${palabraVeces}`);
+  } else {
+    if(numUsu > secretNum){
+      alert("El número es menor");
+
+    }else{
+      alert("El número es mayor");
+    }
+    intentos++;
+    if (intentos > 1) {
+      palabra = "veces";
+    }
+  }
 }
-
-
 
 /*
 alert('¡Bienvenida y bienvenido a nuestro sitio web!');
@@ -37,4 +47,47 @@ edad = prompt('Por favor, ingresa tu edad:');
 if (edad >= 18) {
   alert('¡Puedes obtener tu licencia de conducir!');
 }
+
+
+// Pregunta al usuario qué día de la semana es.
+let diaSemana = prompt("¿Qué día de la semana es?");
+
+// Verifica la respuesta y muestra un mensaje apropiado.
+if (diaSemana === "Sábado" || diaSemana === "Domingo") {
+    alert("¡Buen fin de semana!");
+} else {
+    alert("¡Buena semana!");
+}
+
+
+let numero = parseFloat(prompt("Ingresa un número:"));
+
+// Muestra una alerta informativa sobre el signo del número.
+if (numero > 0) {
+    alert("El número es positivo.");
+} else if (numero < 0) {
+    alert("El número es negativo.");
+} else {
+    alert("El número es cero.");
+}
+
+let puntuacion = parseInt(prompt("Ingresa tu puntuación:"));
+
+// Muestra un mensaje según la puntuación.
+if (puntuacion >= 100) {
+    alert("¡Felicidades, has ganado!");
+} else {
+    alert("Intenta nuevamente para ganar.");
+}
+
+let saldoCuenta = 500; // Reemplaza con el saldo real del usuario.
+
+// Muestra un mensaje utilizando un template string.
+alert(`Tu saldo actual es: $${saldoCuenta.toFixed(2)}`);
+
+let nombreUsuario = prompt("Ingresa tu nombre:");
+
+// Muestra una alerta de bienvenida usando el nombre ingresado.
+alert(`¡Bienvenido, ${nombreUsuario}!`);
+
 */
